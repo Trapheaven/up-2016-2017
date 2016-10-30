53165c00eb2472d7c4361f0c90c5d3b2292ede09
Trapheaven <adpanayotov@mail.bg>
2016-10-30 20:42:59 +0200

    tast1

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int number;
+    cin >> number;
+
+    int reversed = 0;
+    while (number != 0)
+    {
+        int currentDigit = number % 10;
+        reversed = reversed * 10; // всички цифри с една наляво
+        reversed += currentDigit;
+        number /= 10;
+    }
+    cout << reversed << endl;
+
     return 0;
 }
